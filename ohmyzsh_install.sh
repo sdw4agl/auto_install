ccb8ba676a3fb815388a7a6d027e479fb7513583
sdw4agl <chncad@163.com>
2022-11-26 01:06:59 +0800

    Delete ohmyzsh_install.sh

--- ohmyzsh_install.sh
+++
@@ -1,9 +0,0 @@
-#！/bin/bash
-apt update
-apt -y upgrade
-apt install -y zsh
-apt install -y tmux
-chsh -s /bin/zsh
-wget https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh
-chmod +755 install.sh
-bash ./install.sh
