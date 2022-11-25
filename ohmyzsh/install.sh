07ae3b5f5fe460d8c59ddac210d314d60f0240a9
sdw4agl <chncad@163.com>
2022-11-26 01:04:50 +0800

    Update install.sh

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -1,1 +1,9 @@
-
+#！/bin/bash
+apt update
+apt -y upgrade
+apt install -y zsh
+apt install -y tmux
+chsh -s /bin/zsh
+wget https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh
+chmod +755 install.sh
+bash ./install.sh
