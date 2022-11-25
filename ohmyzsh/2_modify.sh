c46844ea842a6a77e2270dc31dee21a68bf9ca40
sdw4agl <chncad@163.com>
2022-11-26 05:36:31 +0800

    Update 2_modify.sh

--- ohmyzsh/2_modify.sh
+++ ohmyzsh/2_modify.sh
@@ -6,20 +6,3 @@ mkdir incr
 cd incr
 wget http://mimosa-pudica.net/src/incr-0.2.zsh
 sed -i '75asource ~/.oh-my-zsh/plugins/incr/incr*.zsh' ~/.zshrc
-while true
-do
-	read -r -p "Are You Want to Reboot? [Y/n] " input
-	case $input in
-	    [yY][eE][sS]|[yY])
-			echo "Reboot now"
-			reboot
-			;;
-	    [nN][oO]|[nN])
-			echo "Don't Reboot"
-			exit 1	       	
-			;;
-	    *)
-			echo "Invalid input..."
-			;;
-	esac
-done
